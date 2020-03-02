66e0210dc63e31b8993da67cfdb991c44a7f1597
Sri Krishna Mannem <srikrishnamannem@Sris-MBP.connectify>
2020-03-02 22:05:53 +0100

    Actual working setup with CRUDRepo with Postgresql

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class DemoApplication {
 	
 	final static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
